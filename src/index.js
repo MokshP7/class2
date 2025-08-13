add36b4da038fa204990e8ef0d72051865ab6d0b
MokshP7 <mokmis231@gmail.com>
2025-08-13 19:35:04 +1000

    class2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Tree from './App.js';
 import reportWebVitals from './reportWebVitals';
-
+class Student extends React.Component {
+  render() 
+  {
+return <h2>Hi my name is Moksh</h2>;
+  }
+}
+function Teacher() {
+  return <h2>Hi,I am Moksh,Teacher Thangamani</h2>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+const root3 = ReactDOM.createRoot(document.getElementById('root3'));
+root.render(<Student />);
+root2.render(<Teacher />);
+root3.render(
   <React.StrictMode>
-    <App />
+    <Tree />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
